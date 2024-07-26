91383fa1afb8f090d5b5ed66d07c3a357b657eba
jimohsegun1 <jimohsegunj@gmail.com>
2024-07-26 13:27:59 +0100

    showing client component in server component

--- src/app/snippets/[id]/edit/page.tsx
+++ src/app/snippets/[id]/edit/page.tsx
@@ -1,13 +1,26 @@
+import { db } from "@/db"
+import { notFound } from "next/navigation"
+import SnippetEditForm from "@/components/snippet-edit-form"
+
 interface SnippetEditPageProps {
     params: {
         id: string
     }
 }
 
-export default function SnippetEditPage(props: SnippetEditPageProps) {
+export default async function SnippetEditPage(props: SnippetEditPageProps) {
     const id = parseInt(props.params.id)
+    const snippet = await db.snippet.findFirst({
+        where: { id }
+    })
+
+    if (!snippet) {
+        return notFound()
+    }
 
     return (
-        <div>SnippetEditPage with id {id}</div>
+        <div>
+            <SnippetEditForm snippet={snippet} />
+        </div>
     )
 }
